fa968de47c577f91403279059b0fe8c73dd8070f
sbeaury <sbeaury@gmail.com>
2022-05-14 18:01:35 -0400

    fix: fix missing title on deploy

--- utils/index.ts
+++ utils/index.ts
@@ -6,4 +6,4 @@ export const buildUrl = (url: string, ...names: string[]): string => {
 };
 
 export const capitalize = (stringToCapitalize: string) =>
-  stringToCapitalize?.charAt(0).toUpperCase() + stringToCapitalize?.slice(1);
+  stringToCapitalize.charAt(0).toUpperCase() + stringToCapitalize.slice(1);
