b4720a6f4c30121ecfdeffdf8249f28e397fab83
sbeaury <sbeaury@gmail.com>
2022-05-14 17:21:07 -0400

    chore: change enums to pascal case

--- types/enums/index.ts
+++ types/enums/index.ts
@@ -1,8 +1,12 @@
 export enum URL {
-  BASE_API_URL = "https://pokeapi.co/api/v2/pokemon",
-  BASE_ASSET_URL = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail",
+  BaseApiURL = "https://pokeapi.co/api/v2/pokemon",
+  BaseAssetURL = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail",
 }
 
-export enum PAGE_TITLE {
-  HOME = "PokēNext",
+export enum PageTitle {
+  Home = "PokēNext",
+}
+
+export enum PokemonAttributes {
+  MaxStats = 255,
 }
