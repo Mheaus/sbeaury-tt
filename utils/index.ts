3432c80c9e9d16cb3f3676ee01150d01cb725d7f
sbeaury <sbeaury@gmail.com>
2022-05-14 17:21:34 -0400

    chore: introduce capitalize util function

--- utils/index.ts
+++ utils/index.ts
@@ -4,3 +4,6 @@ export const buildUrl = (url: string, ...names: string[]): string => {
   if (!names.length) return url;
   return `${url}/${buildChildUrl(...names)}`;
 };
+
+export const capitalize = (stringToCapitalize: string) =>
+  stringToCapitalize?.charAt(0).toUpperCase() + stringToCapitalize?.slice(1);
