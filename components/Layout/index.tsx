f5a64db34d7a348b9dc26e7d9c0addc9737170a0
sbeaury <sbeaury@gmail.com>
2022-05-14 18:25:18 -0400

    chore: cleanup unused import

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import Head from "next/head";
 import Link from "next/link";
 import { PageTitle } from "../../types/enums";
-import { capitalize } from "../../utils";
 
 interface ILayoutProps {
   children: React.ReactNode;
